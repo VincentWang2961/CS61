209192d0cd9d845ebfe163e022999901cdf3f567
VincentWang2961 <vincentwang2961@gmail.com>
2025-03-18 11:46:08 +0800

    RDMtest, Simple Comparison Test

---
+++ CS61B/LabsProjs/skeleton-sp21/lab3/randomizedtest/testThreeAddThreeRemove.java
@@ -0,0 +1,28 @@
+package randomizedtest;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class testThreeAddThreeRemove {
+    @Test
+    public void testThreeAddThreeRemove() {
+        // Init both lists
+        AListNoResizing<Integer> L = new AListNoResizing<>();
+        BuggyAList<Integer> B = new BuggyAList<>();
+
+        // Add 3 items to each list
+        L.addLast(4);
+        B.addLast(4);
+        L.addLast(5);
+        B.addLast(5);
+        L.addLast(6);
+        B.addLast(6);
+        assertEquals(L.getLast(), B.getLast());
+        assertEquals(L.size(), B.size());
+
+        // Remove 3 items from each list
+        assertEquals(L.removeLast(), B.removeLast());
+        assertEquals(L.removeLast(), B.removeLast());
+        assertEquals(L.removeLast(), B.removeLast());
+    }
+}
