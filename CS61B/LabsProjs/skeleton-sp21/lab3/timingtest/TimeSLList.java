f1e991af5f6fcca1f0ccb3dfaf12847ebf9e0314
VincentWang2961 <vincentwang2961@gmail.com>
2025-03-18 11:30:38 +0800

    SLlist timing tests performed, it's outcome the same of the demo :)

--- CS61B/LabsProjs/skeleton-sp21/lab3/timingtest/TimeSLList.java
+++ CS61B/LabsProjs/skeleton-sp21/lab3/timingtest/TimeSLList.java
@@ -23,6 +23,34 @@ public class TimeSLList {
 
     public static void timeGetLast() {
         // TODO: YOUR CODE HERE
+        // Create all 3 columns (N, time, opCount) and a test list
+        AList<Integer> Ns = new AList<>();
+        AList<Double> times = new AList<>();
+        AList<Integer> opCounts = new AList<>();
+        int[] testingList = {1000, 2000, 4000, 8000, 16000, 32000, 64000, 128000};
+        int M = 10000;
+
+        for (int testingNum = 0; testingNum < testingList.length; testingNum += 1){
+            // To get the actual data and it's running time
+            SLList<Integer> L = new SLList<>();
+            // Add N items to the list
+            for (int i = 0; i < testingList[testingNum]; i += 1){
+                L.addLast(0);
+            }
+            // Perform get last M times and get it's running time
+            Stopwatch sw = new Stopwatch();
+            for (int i = 0; i < M; i += 1){
+                L.getLast();
+            }
+            Double timeInSeconds = sw.elapsedTime();
+
+            // Add the data to the columns
+            Ns.addLast(testingList[testingNum]);
+            times.addLast(timeInSeconds);
+            opCounts.addLast(M);
+        }
+
+        printTimingTable(Ns, times, opCounts);
     }
 
 }
